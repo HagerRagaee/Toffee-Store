14b631567dc7c1e73fb319cfda7ac85178750b33
HagerRagaee <102195114+HagerRagaee@users.noreply.github.com>
2023-10-17 15:27:56 +0300

    Update main.java

--- Toffee Store/main.java
+++ Toffee Store/main.java
@@ -1,1 +1,65 @@
 
+import java.util.Scanner;
+
+public class main {
+   
+   /** 
+    * @param email
+    * @throws Exception
+    */
+   public static void  Run(String email) throws Exception{
+    while (true)
+    {
+      int choice ;
+      Boolean checkSignUp = false;  
+      methods method = new methods() ; 
+      Scanner input = new Scanner(System.in) ; 
+      System.out.println("*****************Enter Your chioce*****************"); 
+      System.out.println("1- Visiting Our Store");
+      System.out.println("2- place an order");
+      System.out.println("0- Exit APP");
+
+        
+      choice = input.nextInt() ; 
+
+      if (choice == 0 )
+      {
+          break ; 
+      }
+  
+     else if(choice == 1){
+        method.diplayCatalog();
+      }
+      else if(choice == 2){
+          method.Placeorder(email);
+      }
+
+  }
+}
+  /**
+   * @param arg
+   * @throws Exception
+   */
+    public static void main(String[]arg) throws Exception
+    {
+     Scanner input = new Scanner(System.in) ; 
+      System.out.println("*****************Welcom to Toffee Store**************\nPress\n1-Sign Up\n2-Login\n3-Complete as general User"
+    ); 
+    int option ;
+    Boolean checkSignUp = false;  
+    methods method = new methods() ; 
+    option = input.nextInt();
+    if(option == 1){
+    
+      String em = method.signUpmethod();
+       Run(em);
+      
+    }
+    else if(option == 2){
+     String em = method.logInMethod();
+         Run(em);
+    }
+    else if(option == 3)
+         method.diplayCatalog();                 
+  }
+}
